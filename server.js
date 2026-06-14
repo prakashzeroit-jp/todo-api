8804140a4317c152f2112d9402c244429004bdc2
praakash7542-jp <jyotimaurya7542@gmail.com>
2026-06-14 18:06:22 +0530

    create basic server

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "pppppppppppppp....." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server run at : ${PORT}`);
+});
